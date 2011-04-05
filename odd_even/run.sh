4edf2635ae3342d1927851dcc67a90eff70a2977
Sumin Byeon <suminb@gmail.com>
2011-04-05 05:03:34 -0700

    Grading script seems working.

--- odd_even/run.sh
+++ odd_even/run.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+
+E=$(printf '\033')      # escape <E>
+R="${E}[0m"             # reset  <E>[0m
+
+trim() { echo $1; }
+
+passed=0
+total=$(trim $(ls test*.in|wc -l))
 exec=$1
-for testcase in "test*.in"; do
-    echo "Running against ${testcase}..."
-    useranswer=$(cat $testcase | $exec)
+for testcase in test*.in; do
+    echo -n "Running against ${testcase}..."
+    useranswer=$(trim $(cat $testcase | $exec))
+    answer=$(trim $(cat $(basename $testcase .in).out))
     
-    answer=$(cat $(basename $testcase .in).out)
-    echo "$useranswer $answer"
+    if [ $answer = $useranswer ]; then
+        echo -e "${E}[32mOK${R}"
+        passed=`expr $passed + 1`
+    else
+        echo -en "${E}[31mMismatch${R}"
+        echo " (Expected '${answer}', but '${useranswer}' was returned)"
+    fi
+    
+    #echo "$useranswer $answer"
 done
+
+echo "${passed}/${total}"
